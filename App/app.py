70284ded3a4b669df3d0ebef6543152c2cd3be2c
Pradhyuman <superpradhyuman@gmail.com>
2022-03-27 10:44:43 +0530

    all integerated

--- App/app.py
+++ App/app.py
@@ -78,6 +78,9 @@ def index():
 def login():
     return render_template("login.html")
 
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 @app.route("/about")
 def about():
     return render_template("about.html")
